65e4151809ada252f4c40b0e883c6ebc40bdd083
yura4350 <yura435012@gmail.com>
2021-12-21 22:58:20 +0300

    Нашел правильно работающую функцию resize, использовал её в программе checker

--- main.py
+++ main.py
@@ -4,21 +4,22 @@ import numpy as np
 import cv2 as cv
 q = []
 # параметры цветового фильтра
-hsv_min = np.array([10, 100, 20])
-hsv_max = np.array([20, 255, 255])
+hsv_min = np.array([11, 100, 20])
+hsv_max = np.array([12, 255, 255])
 
 k = 0
 EPS = 33
 
 fn = 'photos/orange_0.jpg' # путь к файлу с картинкой
 
-img = cv.imread(fn, cv.IMREAD_UNCHANGED)
+img = cv.imread(fn)
+
 print(img.shape)
 #cale_percent = 20
 #width = int(img.shape[1] * scale_percent / 100)
 #height = int(img.shape[0] * scale_percent / 100)
 #dsize = (width, height)
-#img = img.resize(img, int(img.shape[1] / 5), int(img.shape[0] / 5))
+img = img.resize(img, (int(img.shape[1] / 5), int(img.shape[0] / 5)))
 
 
 hsv = cv.cvtColor( img, cv.COLOR_BGR2HSV ) # меняем цветовую модель с BGR на HSV
@@ -34,7 +35,7 @@ cv.drawContours( img, contours, -1, (255,0,0), 3, cv.LINE_AA, hierarchy, 1 )
 cv.imshow('contours', img) # выводим итоговое изображение в окно
 for i in range(len(contours)):
     #print(cv.contourArea(contours[i]))
-    if cv.contourArea(contours[i]) > 300:
+    if cv.contourArea(contours[i]) > 1000:
 
         print('Ученик, дневной пансион')
 
