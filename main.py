5ad0cc3126138ee6a23200baf55289f748d5caaf
yura4350 <yura435012@gmail.com>
2021-12-21 23:21:56 +0300

    В main-е определили пороговые цвета для бейджиков дневного пансиона (оранжевого цвета)

--- main.py
+++ main.py
@@ -1,20 +1,15 @@
 import sys
 import math
 import numpy as np
-import cv2 as cv
+import cv2
 q = []
 # параметры цветового фильтра
-hsv_min = np.array([11, 100, 20])
-hsv_max = np.array([12, 255, 255])
+hsv_min = np.array([12, 100, 20])
+hsv_max = np.array([15, 255, 255])
 
 k = 0
 EPS = 33
-
-
-
-
-
-src = cv.imread('orange_0.jpg', cv.IMREAD_UNCHANGED)
+src = cv2.imread('photos/orange_2.jpg', cv2.IMREAD_UNCHANGED)
 
 scale_percent = 20
 width = int(src.shape[1] * scale_percent / 100)
@@ -22,31 +17,24 @@ height = int(src.shape[0] * scale_percent / 100)
 
 dsize = (width, height)
 
-img = cv.resize(src, dsize, interpolation=cv.INTER_AREA)
-
-
-print(img.shape)
-
-
-
-hsv = cv.cvtColor( img, cv.COLOR_BGR2HSV ) # меняем цветовую модель с BGR на HSV
-thresh = cv.inRange( hsv, hsv_min, hsv_max ) # применяем цветовой фильтр
+img = cv2.resize(src, dsize, interpolation=cv2.INTER_AREA)
+hsv = cv2.cvtColor( img, cv2.COLOR_BGR2HSV ) # меняем цветовую модель с BGR на HSV
+thresh = cv2.inRange( hsv, hsv_min, hsv_max ) # применяем цветовой фильтр
 
 
     # ищем контуры и складируем их в переменную contours
-contours, hierarchy = cv.findContours(thresh.copy(), cv.RETR_TREE, cv.CHAIN_APPROX_SIMPLE)
+contours, hierarchy = cv2.findContours(thresh.copy(), cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
 
     # отображаем контуры поверх изображения
 
-cv.drawContours( img, contours, -1, (255,0,0), 3, cv.LINE_AA, hierarchy, 1 )
-cv.imshow('contours', img) # выводим итоговое изображение в окно
+cv2.drawContours( img, contours, -1, (255,0,0), 3, cv2.LINE_AA, hierarchy, 1 )
+cv2.imshow('contours', img) # выводим итоговое изображение в окно
 for i in range(len(contours)):
     #print(cv.contourArea(contours[i]))
-    if cv.contourArea(contours[i]) > 1000:
+    if cv2.contourArea(contours[i]) > 300:
 
         print('Ученик, дневной пансион')
 
 
 
-cv.waitKey(0)
-
+cv2.waitKey(0)
--- photos/checker.py
+++ photos/checker.py
@@ -1,4 +1,14 @@
+import sys
+import math
+import numpy as np
 import cv2
+q = []
+# параметры цветового фильтра
+hsv_min = np.array([11, 100, 20])
+hsv_max = np.array([12, 255, 255])
+
+k = 0
+EPS = 33
 src = cv2.imread('orange_0.jpg', cv2.IMREAD_UNCHANGED)
 
 scale_percent = 20
@@ -7,6 +17,25 @@ height = int(src.shape[0] * scale_percent / 100)
 
 dsize = (width, height)
 
-output = cv2.resize(src, dsize, interpolation=cv2.INTER_AREA)
-cv2.imshow('Image', output)
+img = cv2.resize(src, dsize, interpolation=cv2.INTER_AREA)
+hsv = cv2.cvtColor( img, cv2.COLOR_BGR2HSV ) # меняем цветовую модель с BGR на HSV
+thresh = cv2.inRange( hsv, hsv_min, hsv_max ) # применяем цветовой фильтр
+
+
+    # ищем контуры и складируем их в переменную contours
+contours, hierarchy = cv2.findContours(thresh.copy(), cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+
+    # отображаем контуры поверх изображения
+
+cv2.drawContours( img, contours, -1, (255,0,0), 3, cv2.LINE_AA, hierarchy, 1 )
+cv2.imshow('contours', img) # выводим итоговое изображение в окно
+for i in range(len(contours)):
+    #print(cv.contourArea(contours[i]))
+    if cv2.contourArea(contours[i]) > 1000:
+
+        print('Ученик, дневной пансион')
+
+
+
 cv2.waitKey(0)
+
